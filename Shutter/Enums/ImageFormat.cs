cc6346c7e1a6fd8aedd72986626f48f216be9e17
rthomasv3 <39268276+rthomasv3@users.noreply.github.com>
2025-08-27 06:07:05 -0500

    add missing documentation

--- Shutter/Enums/ImageFormat.cs
+++ Shutter/Enums/ImageFormat.cs
@@ -5,6 +5,13 @@
 /// </summary>
 public enum ImageFormat
 {
+    /// <summary>
+    /// Uses <see cref="SixLabors.ImageSharp.Formats.Png.PngEncoder"/> for image encoding.
+    /// </summary>
     Png,
+
+    /// <summary>
+    /// Uses <see cref="SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder"/> for image encoding.
+    /// </summary>
     Jpeg
 }
